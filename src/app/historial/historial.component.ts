d99b8872b3f9e5ba30805154f72fa9f1d82bc017
jhon <jhonm9696@gmail.com>
2022-09-26 17:41:33 -0500

    Creacion tabla historial, implementacion de routerlink

--- src/app/historial/historial.component.ts
+++ src/app/historial/historial.component.ts
@@ -17,23 +17,31 @@ export class HistorialComponent implements OnInit {
   historialIcon=faClockRotateLeft
   motivosIcon=faMoneyCheck
 
+  historial: any = [
+    {
+      fecha: { date: '2022/08/12' },
+      hora: '12:00',
+      tipo:'transferencia',
+      monto: '-160',
+      origen_destino:'josefer1472@gmail.com',
+      motivo:'diversion',
+
+      fecha1: { date: '2022/08/12' },
+      hora1: '22:25',
+      tipo1:'deposito',
+      monto1: '+400',
+      origen_destino1:'573678769865',
+      motivo1:'pagos',
+    },
+  ];
+
+
+
   constructor(private router:Router,private auth:AuthService) { }
 
   ngOnInit(): void {
   }
 
-  trasferenciasRoute() {
-    this.router.navigate(['/transaccion']);
-    }
-    contactoRoute() {
-      this.router.navigate(['/contacto']);
-      }
-    historialRoute() {
-      this.router.navigate(['/historial']);
-      }
-    motivosRoute() {
-      this.router.navigate(['/motivos']);
-      }
       logout(){
         this.router.navigate(['']);
         this.auth.logout()
