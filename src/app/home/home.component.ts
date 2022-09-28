243f9f2bbfbf5704dda1a2e9eff43cb81d5ef15d
jhon <jhonm9696@gmail.com>
2022-09-28 15:24:48 -0500

    correccion texto de alertas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -91,8 +91,8 @@ export class HomeComponent implements OnInit {
   }
   alertaEliminarwallet() {
     this.alertsService.confirm({
-      title: '¿Desea eliminar su wallet?',
-      text: `"Lamentamos que quieras abandonarnos tan pronto y que no puedas seguir disfrutando de la simplicidad, seguridad y trazabilidad de MyWallet. Si quieres continuar con el proceso de cierre, da clic en SI, de lo contrario da click en cancelar`,
+      title: '¿Estás seguro que quieres cancelar tu cuenta?',
+      text: `Lamentamos que quieras abandonarnos tan pronto y que no puedas seguir disfrutando de la simplicidad, seguridad y trazabilidad de MyWallet. Si quieres continuar con el proceso de cierre, da clic en SI, de lo contrario da click en cancelar`,
       bodyDeConfirmacion: 'Tu solicitud se encuentra en proceso',
       tituloDeConfirmacion: 'EN PROCESO',
       bodyDelCancel: 'Que gusto que desees continuar con Nosotros',
